626535142f60c40bf5c35f4418deaa64c58ed659
m4rni <marnisu66@gmail.com>
2017-06-02 21:15:33 +0700

    bebas menentukan method abstrak di interface

---
+++ Sapi.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hewan;
+
+
+public class Sapi implements Music{
+    @Override
+    
+    public void bernyanyi(){
+        System.out.println("Moooow.... Mooow...");
+    }
+    
+}
