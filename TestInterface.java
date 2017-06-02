b55bcdc5066480a99307519e1d21f8074063c134
m4rni <marnisu66@gmail.com>
2017-06-02 21:17:38 +0700

    pembuktian

---
+++ TestInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hewan;
+
+
+public class TestInterface {
+    public static void main(String[]args){
+        Sapi cow = new Sapi();
+        Manusia human = new Manusia();
+        cow.bernyanyi();
+        human.bernyanyi();
+    }
+    
+}
